67a8bdeb19ee9551165eb1dceb68fb67ad968884
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2017-07-14 09:39:21 -0700

    Checkpoint CSS
    Todo:
    Color Correct from purple to coral - over ride milligram
    Save button text center- over ride milligram padding
    Resize bage Save button Shift

--- src/components/OptionsDisplay.js
+++ src/components/OptionsDisplay.js
@@ -21,14 +21,14 @@ class OptionsDisplay extends Component {
         let drugName = this.props.displayOptions[obj];
         console.log(obj, drugName);
         return(
-            <form className="optionDisplayForm" key={obj}> 
-
-                <input className="optionDisplaySubmit" value="save" type="submit" onClick={e => {
+            <form className="optionsDisplayForm" key={obj}> 
+                
+                <input className="optionsDisplaySubmit" value="save" type="submit" onClick={e => {
                     this.handleOnClick(e);
                     }}
                 />
                 
-                <div className="optionDisplayFormInfo"> {drugName} </div>
+                <div className="optionsDisplayFormInfo"> {drugName} </div>
 
                 <input type="hidden" value={drugName} ref={input => {
                         this.drugName = input
@@ -43,7 +43,7 @@ class OptionsDisplay extends Component {
         console.log("displayOptions O.D.", this.props.displayOptions);
         return(
             <div className="optionsDisplayDiv">
-                
+                <div className="optionsDisplaySpacer"> </div>
                 {Object.keys(this.props.displayOptions).map( obj => {
                     return this.renderOptionForm(obj);
                 })}
